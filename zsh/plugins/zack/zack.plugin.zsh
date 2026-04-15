3c1eac4f4a436b3772dcfa08fa4fbe35251cfeac
Zack Sheppard <zack@zacksheppard.com>
2026-04-15 11:31:01 +0100

    Update the zack plugin

--- zsh/plugins/zack/zack.plugin.zsh
+++ zsh/plugins/zack/zack.plugin.zsh
@@ -3,15 +3,11 @@
 
 # Shared-shell (bash or zsh or...) terminal tweaks and productivity commands
 
-# Up a directory
-alias up="cd .."
-
 # Set up better alias commands for ls/ll
 alias ls='ls -G'
 alias ll='ls -hl'
 
 # Git "frequently used commands"
-alias grid="git rebase develop"
 alias gs="git status"
 alias gg="git grep"
 
@@ -19,19 +15,15 @@ alias gg="git grep"
 alias glow="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 
 # Push current branch to origin without git's "set upstream!" annoyances
-alias gpb="git rev-parse --abbrev-ref HEAD | xargs -n 1 git push origin"
+# alias gpb="git rev-parse --abbrev-ref HEAD | xargs -n 1 git push origin"
 
 # Push current branch to origin and open a pull request (requires github's hub command)
-alias gipper="gpb && open \`hub pull-request\`"
+alias gipper="ggp && open \`hub pull-request\`"
 
-function squash-killer { git checkout -q $1 && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base develop $branch) && [[ $(git cherry develop $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done }
-
-alias squash-killer-main='git checkout -q main && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
+#function squash-killer { git checkout -q $1 && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base develop $branch) && [[ $(git cherry develop $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done }
+function squash-killer-main { git checkout -q $1 && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done }
 
 alias be='bundle exec'
-
-# CD and immediately LS a directory
-function cdl { cd $1; ls; }
 
 # Define UTF-8 as our terminal language to ensure emoji support!
 export LC_ALL=en_US.UTF-8
